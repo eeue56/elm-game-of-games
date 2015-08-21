a0d7a086ee2037b69933674de38eea988071d2b1
eeue56 <eep41b@bangor.ac.uk>
2015-08-21 15:36:43 +0100

    add living neighbors calc

--- App.elm
+++ App.elm
@@ -5,6 +5,19 @@ import Array exposing (..)
 main =
   show <| update board 2 2 (\_ -> 5)
 
+livingNeighbours : Array (Array a) -> Int -> Int ->Int
+livingNeighbours array i j = 
+  let 
+    guardedGet = (\x y -> if x /= j and y /= j then matrixGet array x else Nothing)
+  sum 
+    List.map (\x -> 
+      case x of 
+        Just _ -> 1 
+        Nothing -> 0)
+    <| List.map (\x -> List.map (guardedGet x) [i-1..i+1]) [j-1..j+1] 
+
+matrixGet : Array (Array a) -> Int -> Int -> Maybe a 
+matrixGet array i j = Array.get i <| Array.get j array 
 
 update array i j f = 
   let 
