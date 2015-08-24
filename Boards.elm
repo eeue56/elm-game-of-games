8ff38f767ba72d2f4c706c2640618c555b8cb0d7
eeue56 <eep41b@bangor.ac.uk>
2015-08-25 00:30:17 +0100

    add a module for board defaults

---
+++ Boards.elm
@@ -0,0 +1,15 @@
+module Boards where
+
+import Array
+
+lonelyBoard = 
+  Array.fromList
+    <| List.map Array.fromList
+    <|
+      [ [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0] ]
+
+emptyBoard = Array.fromList <|  List.map (\x -> Array.repeat 5 0) [0..4] 
+
+fullBoard = Array.fromList <|  List.map (\x -> Array.repeat 5 1) [0..4] 
