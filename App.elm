771fc00c271ae3f20809a409e1d126058062cbe4
Maslab eeePC <blacksheepmails@gmail.com>
2015-08-27 21:55:50 +0100

    can now click squares to make patterns. but in vertical pos of mouse

--- App.elm
+++ App.elm
@@ -10,62 +10,85 @@ import Boards
 
 import Color exposing (red, black, toRgb, rgb)
 
-type Update = NextStep Bool | Reset | Noop
+type Update = MouseClick Int Int | NextStep Bool | Reset | Noop
 type alias Board = Array (Array Int)
+type alias Model = {board : Board, clicks : Int, debug : String}
 
-pageWidth = 1000
-pageHeight = 750
+collageWidth = 1100
+collageHeight = 400
+boardWidth = 15
+boardHeight = 12
 
 rectSize = 25
 
 board' : Board
-board' =  Boards.stampBoard 1 9 Boards.noahsLessBeautifulGithubAvatar <| Boards.emptyBoard 10 15
+board' =  Boards.stampBoard 1 3 Boards.noahsLessBeautifulGithubAvatar <| Boards.emptyBoard boardWidth boardHeight
 
+model : Model
 model = {
   board = board',
-  clicks = 0 }
+  clicks = 0,
+  debug = "" }
 
 
 resetModel model = 
   let
     resetBoard model = { model | board <- board'}
     resetClick model = { model | clicks <- 0 }
+    resetDebug model = { model | debug <- ""}
   in
     resetBoard <| resetClick model
 
-view model = above (container 500 100 middle <| show model.clicks) <| draw model.board -- <| arrayUpdate model.board livingNeighbours
+view model = 
+  below (container 1000 20 middle <| show model.debug) <| below (container 500 20 middle <| show model.clicks) <| draw model.board  -- <| arrayUpdate model.board livingNeighbours
 
 addClick model = { model | clicks <- model.clicks + 1 }
+addDebug : String -> Model -> Model
+addDebug msg model = { model | debug <- msg }
 
+updateModel : Update -> Model -> Model
 updateModel action model =
   case action of
     NextStep bool -> if
       | bool -> addClick { model | board <- gameStep model.board }
       | otherwise -> model
+    MouseClick x y -> addDebug (toString <| toSquare x y) { model | board <- toggle model.board <| toSquare x y }
     Reset -> resetModel model
     _ -> model
 
-stepSignal = 
-  Signal.merge 
-    (Signal.map (NextStep) Mouse.isDown)
-    (Signal.map NextStep <| Keyboard.isDown 78)
+clickSignal = Signal.map2 (\isDown (x,y) -> if isDown then MouseClick x y else Noop) Mouse.isDown Mouse.position
 
+model' : Signal Model
 model' =
   Signal.foldp
     updateModel
     model
-    <| Signal.merge 
-        stepSignal
+    <| Signal.mergeMany
+      [
+        clickSignal,
+        (Signal.map NextStep <| Keyboard.isDown 78),
         (Signal.map (\_ -> Reset) <| Keyboard.isDown 82)
+      ]
      
 
-main = Signal.map (view) model'
+main = Signal.map view model'
+
+toSquare : Int -> Int -> (Int, Int)
+toSquare x y = 
+  (
+    x // rectSize,
+    y // rectSize
+  )
 
 drawRect : Float -> Float -> Int -> Form
-drawRect x y value =
-  rect rectSize rectSize
-    |> filled (rgb (value * 255) 0 0 )
-    |> move (rectSize * x, (pageHeight/2)-(rectSize * y))
+drawRect x y value = 
+  let 
+    originX = -1* (collageWidth/2) + rectSize/2
+    originY = (collageHeight/2) + rectSize/2
+  in
+    rect rectSize rectSize
+      |> filled (rgb (value * 255) 0 0 )
+      |> move (originX + rectSize * x, originY - (rectSize * y))
 
 drawRow j rowArray =
   let
@@ -73,17 +96,21 @@ drawRow j rowArray =
   in
      Array.toList <| Array.indexedMap rekt rowArray
 
+drawArray : Board -> List Form
 drawArray array =
   (List.concat << Array.toList) <| Array.indexedMap (drawRow) array
 
 
 draw : Board -> Element
 draw array = 
-  collage pageWidth pageHeight
-    <| drawArray array
+  collage collageWidth collageHeight
+    <| (filled (rgb 255 200 255) <| rect collageWidth collageHeight) :: drawArray array
 
 arrayUpdate : Board -> (Board -> Int -> Int -> Int) -> Board
 arrayUpdate array f = Array.indexedMap (\y n -> Array.indexedMap (\x m -> f array x y) n) array
+
+toggle : Board -> (Int, Int) -> Board
+toggle board (i,j) = update board i j (\x -> (x+1)%2 )
 
 gameStep : Board -> Board
 gameStep array = arrayUpdate array gameRule
@@ -121,14 +148,24 @@ matrixGet array i j = case Array.get j array of
   Just x ->  Array.get i x
   Nothing -> Nothing
 
+update m i j f = 
+    let
+        row = case Array.get j m of
+          Just r -> r 
+          Nothing -> Array.empty
+        elt = case Array.get i row of
+          Just e -> e
+          Nothing -> 0
+    in
+        Array.set j (Array.set i (f elt) row) m
 
 
-update array i j f = 
-  let 
-    updatePart x = case Array.get j x of
-      Just y -> Array.set j (Array.set i (f y) x) array
-      Nothing -> array
-  in
-  case Array.get i array of
-    Just x -> updatePart x
-    Nothing -> array
+--update array i j f = 
+--  let 
+--    updatePart x = case Array.get j x of
+--      Just y -> Array.set j (Array.set i (f y) x) array
+--      Nothing -> array
+--  in
+--    case Array.get i array of
+--      Just x -> updatePart x
+--      Nothing -> array
