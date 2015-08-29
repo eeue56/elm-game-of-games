a8fc3720ad6fdf72433ed21c8799817ba4be789b
eeue56 <noah@astraldynamics.co.uk>
2015-08-29 15:03:29 +0100

    optimize drawing array

--- Drawing.elm
+++ Drawing.elm
@@ -23,13 +23,9 @@ drawRect model x y value =
 {-| 
 Assume that the canvas has already been drawn as black
   -}
-fastDrawRect : Model -> Float -> Float -> Int -> Form
-fastDrawRect model x y value = 
-  let 
-    originX = (model.rectSize / 2) - (toFloat model.width / 2) 
-    originY = (toFloat model.height / 2) - model.rectSize / 2
-  in 
-    rect model.rectSize model.rectSize
+fastDrawRect : Model -> Shape -> (Float, Float) -> Float -> Float -> Int -> Form
+fastDrawRect model myRect (originX, originY) x y value = 
+    myRect
       |> filled red
       |> move (originX + model.rectSize * x, originY - (model.rectSize * y))
 
@@ -43,11 +39,31 @@ drawRow model j rowArray =
     <| List.filter (\(_, x) -> x > 0) 
     <| Array.toIndexedList rowArray 
 
+fastDrawRow : Model -> (Float, Float) -> Int -> Array Int -> List Form
+fastDrawRow model (originX, originY) j rowArray =
+  let
+    myRect = rect model.rectSize model.rectSize
+    floatJ = toFloat j
+    rekt (i, v) = (fastDrawRect model myRect (originX, originY)) (toFloat i) (floatJ) (v)
+  in
+    List.map rekt
+    <| List.filter (\(_, x) -> x > 0) 
+    <| Array.toIndexedList rowArray 
 
 
 drawArray : Model -> List Form
 drawArray model =
-  (List.concat << Array.toList) <| Array.indexedMap (drawRow model) model.board
+  (List.concat << Array.toList) 
+    <| Array.indexedMap (drawRow model) model.board
+
+fastDrawArray : Model -> List Form
+fastDrawArray model =
+  let
+    originX = (model.rectSize / 2) - (toFloat model.width / 2) 
+    originY = (toFloat model.height / 2) - model.rectSize / 2
+  in
+    (List.concat << Array.toList) 
+      <| Array.indexedMap (fastDrawRow model (originX, originY)) model.board
 
 background width height =
   rect (toFloat width) (toFloat height)
@@ -56,4 +72,4 @@ background width height =
 --draw : Model -> Element
 draw model = 
   collage model.width model.height
-    <| (background model.width model.height) :: (drawArray model)
+    <| (background model.width model.height) :: (fastDrawArray model)
